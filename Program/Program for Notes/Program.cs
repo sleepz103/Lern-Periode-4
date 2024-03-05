e6e763a45a5b5c13f7ef117e24e463b5e3ddc336
sleepz1 <98420917+sleepz103@users.noreply.github.com>
2024-03-05 10:06:21 +0100

    uploading solution to github
    wonder if i can update code easier now

---
+++ Program/Program for Notes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program_for_Notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programm for Notes");
+
+
+            moveText("New Note",1);
+            moveText("Edit Note");
+            moveText("See Notes");
+
+            /*
+            Console.WriteLine("\nNew Note");
+            Console.WriteLine("Edit Note");
+            Console.WriteLine("See Notes");
+            */
+        }
+        static void moveText(string textInput, int offset = 0)
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(3,currentLineCursor + offset);
+            Console.WriteLine(textInput);
+
+        }
+    }
+}
