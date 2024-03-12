892e20e5799aa98a0d3d04508eec6553528c628a
sleepz1 <98420917+sleepz103@users.noreply.github.com>
2024-03-12 11:08:10 +0100

    Update Program.cs

--- Program/Program for Notes/Program.cs
+++ Program/Program for Notes/Program.cs
@@ -4,54 +4,84 @@ namespace Program_for_Notes
 {
     internal class Program
     {
+        static string[] menuOptions = { "New Note", "Edit Note", "See Notes" };
+        static char[] PointerArray = new char[menuOptions.Length];
+        static int menuSelection = 0;
         static void Main(string[] args)
         {
             //Variables
-            int menuSelection = 0;
-            
+            for (int i = 0; i < PointerArray.Length; i++)
+            {
+                PointerArray[i] = ' ';
+            }
+            char Pointer = '>';
+            bool isUsingMenu = true;
+            PointerArray[0] = Pointer;
+
             //Program
-            Console.WriteLine("Programm for Notes");
-            
+            DisplayMenu();
 
-            moveText("New Note",1);
-            moveText("Edit Note");
-            moveText("See Notes");
+            while (isUsingMenu == true)
+            {
+                ConsoleKeyInfo KeyData = GetKey();
+                if (KeyData.Key.Equals(ConsoleKey.UpArrow) || KeyData.Key.Equals(ConsoleKey.DownArrow))
+                {
+                    if (KeyData.Key.Equals(ConsoleKey.UpArrow))
+                    {
+                        PointerArray[menuSelection] = ' ';
+                        menuSelection--;
+                        if (menuSelection <= 0)
+                        {
+                            menuSelection = 0;
+                        }
+                        PointerArray[menuSelection] = Pointer;
+                    }
+                    if (KeyData.Key.Equals(ConsoleKey.DownArrow))
+                    {
+                        PointerArray[menuSelection] = ' ';
+                        menuSelection++;
+                        if (menuSelection >= 3)
+                        {
+                            menuSelection = menuOptions.Length - 1;
+                        }
+                        PointerArray[menuSelection] = Pointer;
+                    }
+                    DisplayMenu();
+                }
 
-            GetKey();
-            
-            
 
 
-            
-
+            }
         }
         static void moveText(string textInput, int offset = 0)
         {
             int currentLineCursor = Console.CursorTop;
-            Console.SetCursorPosition(3,currentLineCursor + offset);
+            Console.SetCursorPosition(3, currentLineCursor + offset);
             Console.WriteLine(textInput);
 
         }
 
-        static void GetKey()
+        static ConsoleKeyInfo GetKey()
         {
-            System.ConsoleKey DownArrow = ConsoleKey.DownArrow;
-            System.ConsoleKey UpArrow = ConsoleKey.UpArrow;
-
             ConsoleKeyInfo KeyData = Console.ReadKey(true);
-            if(KeyData.Key.Equals(UpArrow))
-            {
-                Console.WriteLine("Input was Up Arrow");
-            }
-            if (KeyData.Key.Equals(DownArrow))
-            {
-                Console.WriteLine("Input was Down Arrow");
-            }
-
+            return KeyData;
         }
 
+        static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Programm for Notes\n");
 
+            Console.WriteLine("{0} {1}", PointerArray[0], menuOptions[0]);
+            Console.WriteLine("{0} {1}", PointerArray[1], menuOptions[1]);
+            Console.WriteLine("{0} {1}", PointerArray[2], (menuOptions[2]));
 
+            /*
+            moveText(menuOptions[0], 1);
+            moveText(menuOptions[1]);
+            moveText(menuOptions[2]);
+            */
+        }
 
     }
 }
