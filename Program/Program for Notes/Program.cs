fc96d5e2b628915621a1a0db6968c6d4644aacdb
sleepz1 <98420917+sleepz103@users.noreply.github.com>
2024-03-12 09:25:57 +0100

    arrow keys decrease or increase int value

--- Program/Program for Notes/Program.cs
+++ Program/Program for Notes/Program.cs
@@ -1,21 +1,44 @@
-﻿namespace Program_for_Notes
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Program_for_Notes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Programm for Notes");
+            //Variables
+            int menuSelection = 0;
+            bool isUsingMenu = true;
+            
 
+            //Program
+            Console.WriteLine("Programm for Notes");
+            
 
             moveText("New Note",1);
             moveText("Edit Note");
             moveText("See Notes");
 
-            /*
-            Console.WriteLine("\nNew Note");
-            Console.WriteLine("Edit Note");
-            Console.WriteLine("See Notes");
-            */
+            
+            while (isUsingMenu == true)
+            {
+                ConsoleKeyInfo KeyData = GetKey();
+                if (KeyData.Key.Equals(ConsoleKey.UpArrow))
+                {
+                    menuSelection++;
+                    Console.WriteLine("Input was Up Arrow "+menuSelection);
+                }
+                if (KeyData.Key.Equals(ConsoleKey.DownArrow))
+                {
+                    menuSelection--;
+                    Console.WriteLine("Input was Down Arrow " + menuSelection);
+                }
+            }
+            
+
+
+            
+
         }
         static void moveText(string textInput, int offset = 0)
         {
@@ -24,5 +47,15 @@
             Console.WriteLine(textInput);
 
         }
+
+        static ConsoleKeyInfo GetKey()
+        {
+            ConsoleKeyInfo KeyData = Console.ReadKey(true);
+            return KeyData;
+        }
+
+
+
+
     }
 }
